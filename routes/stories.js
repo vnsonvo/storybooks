f9c1065ac599f5751e2e921f41acc33b55e7be25
Son Vo <vnsonvo@gmail.com>
2022-08-09 22:19:03 +0700

    delete story

--- routes/stories.js
+++ routes/stories.js
@@ -41,35 +41,56 @@ router.get("/", ensureAuth, async (req, res) => {
 // @desc show edit page
 // @route GET /stories/edit/:id
 router.get("/edit/:id", ensureAuth, async (req, res) => {
-  console.log("aa")
-  const story = await Story.findOne({ _id: req.params.id }).lean()
-  if (!story) {
-    return res.render("error/404")
-  }
-  if (story.user != req.user.id) {
-    res.redirect("/stories")
-  } else {
-    res.render("stories/edit", { story })
+  try {
+    const story = await Story.findOne({ _id: req.params.id }).lean()
+    if (!story) {
+      return res.render("error/404")
+    }
+    if (story.user != req.user.id) {
+      res.redirect("/stories")
+    } else {
+      res.render("stories/edit", { story })
+    }
+  } catch (err) {
+    console.error(err)
+    return res.render("error/500")
   }
 })
 
 // @desc update story
 // @route PUT /stories/:id
 router.put("/:id", ensureAuth, async (req, res) => {
-  let story = await Story.findById(req.params.id).lean()
-  if (!story) {
-    return res.render("error/404")
+  try {
+    let story = await Story.findById(req.params.id).lean()
+    if (!story) {
+      return res.render("error/404")
+    }
+
+    if (story.user != req.user.id) {
+      res.redirect("stories")
+    } else {
+      story = await Story.findOneAndUpdate({ _id: req.params.id }, req.body, {
+        new: true,
+        runValidators: true,
+      })
+
+      res.redirect("/dashboard")
+    }
+  } catch (err) {
+    console.error(err)
+    return res.render("error/500")
   }
+})
 
-  if (story.user != req.user.id) {
-    res.redirect("stories")
-  } else {
-    story = await Story.findOneAndUpdate({ _id: req.params.id }, req.body, {
-      new: true,
-      runValidators: true,
-    })
-
+// @desc Delete story
+// @route DELETE /stories/:id
+router.delete("/:id", ensureAuth, async (req, res) => {
+  try {
+    await Story.remove({ _id: req.params.id })
     res.redirect("/dashboard")
+  } catch (err) {
+    console.error(err)
+    return res.render("error/500")
   }
 })
 
