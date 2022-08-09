6677d18ed667176df21ed71bd093fd50d51005d9
Son Vo <vnsonvo@gmail.com>
2022-08-09 16:17:41 +0700

    user can login and logout now with navbar added

--- routes/auth.js
+++ routes/auth.js
@@ -16,4 +16,15 @@ router.get(
   }
 );
 
+// @desc Logout user
+// @route /auth/logout
+router.get("/logout", function (req, res, next) {
+  req.logout(function (err) {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/");
+  });
+});
+
 module.exports = router;
