35725d62341242d2c834f9add2a2631738780a4d
Son Vo <vnsonvo@gmail.com>
2022-08-09 22:10:42 +0700

    edit story

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const { engine } = require("express-handlebars")
 const passport = require("passport")
 const session = require("express-session")
 const connectDB = require("./config/db")
+const methodOverride = require("method-override")
 const indexRoutes = require("./routes/index")
 const authRoutes = require("./routes/auth")
 const storyRoutes = require("./routes/stories")
@@ -26,19 +27,37 @@ const app = express()
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
+// Method Override
+app.use(
+  methodOverride(function (req, res) {
+    if (req.body && typeof req.body === "object" && "_method" in req.body) {
+      // look in urlencoded POST bodies and delete it
+      let method = req.body._method
+      delete req.body._method
+      return method
+    }
+  })
+)
+
 // Logging
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"))
 }
 
 // Handlebars Helpers
-const { formatDate, stripTags, truncate } = require("./helpers/hbs")
+const {
+  formatDate,
+  stripTags,
+  truncate,
+  editIcon,
+  select,
+} = require("./helpers/hbs")
 
 // Handlebars
 app.engine(
   ".hbs",
   engine({
-    helpers: { formatDate, stripTags, truncate },
+    helpers: { formatDate, stripTags, truncate, editIcon, select },
     defaultLayout: "main",
     extname: ".hbs",
   })
@@ -58,6 +77,12 @@ app.use(
 // Passport middleware
 app.use(passport.initialize())
 app.use(passport.session())
+
+// Set global var
+app.use(function (req, res, next) {
+  res.locals.user = req.user || null
+  next()
+})
 
 // Static folder
 app.use(express.static(path.join(__dirname, "public")))
--- routes/stories.js
+++ routes/stories.js
@@ -38,4 +38,39 @@ router.get("/", ensureAuth, async (req, res) => {
   }
 })
 
+// @desc show edit page
+// @route GET /stories/edit/:id
+router.get("/edit/:id", ensureAuth, async (req, res) => {
+  console.log("aa")
+  const story = await Story.findOne({ _id: req.params.id }).lean()
+  if (!story) {
+    return res.render("error/404")
+  }
+  if (story.user != req.user.id) {
+    res.redirect("/stories")
+  } else {
+    res.render("stories/edit", { story })
+  }
+})
+
+// @desc update story
+// @route PUT /stories/:id
+router.put("/:id", ensureAuth, async (req, res) => {
+  let story = await Story.findById(req.params.id).lean()
+  if (!story) {
+    return res.render("error/404")
+  }
+
+  if (story.user != req.user.id) {
+    res.redirect("stories")
+  } else {
+    story = await Story.findOneAndUpdate({ _id: req.params.id }, req.body, {
+      new: true,
+      runValidators: true,
+    })
+
+    res.redirect("/dashboard")
+  }
+})
+
 module.exports = router
