06e8222996d1e8afe6b86a323837d13a25f695e0
Son Vo <vnsonvo@gmail.com>
2022-08-09 22:27:41 +0700

    show user stories

--- routes/stories.js
+++ routes/stories.js
@@ -38,6 +38,21 @@ router.get("/", ensureAuth, async (req, res) => {
   }
 })
 
+// @desc Show single story
+// @route GET /stories/:id
+router.get("/:id", ensureAuth, async (req, res) => {
+  try {
+    let story = await Story.findById(req.params.id).populate("user").lean()
+    if (!story) {
+      return res.render("error/404")
+    }
+    res.render("stories/show", { story })
+  } catch (err) {
+    console.error(err)
+    res.render("error/404")
+  }
+})
+
 // @desc show edit page
 // @route GET /stories/edit/:id
 router.get("/edit/:id", ensureAuth, async (req, res) => {
@@ -91,6 +106,24 @@ router.delete("/:id", ensureAuth, async (req, res) => {
   } catch (err) {
     console.error(err)
     return res.render("error/500")
+  }
+})
+
+// @desc User stories
+// @route GET /stories/user/:userId
+router.get("/user/:userId", ensureAuth, async (req, res) => {
+  try {
+    const stories = await Story.find({
+      user: req.params.userId,
+      status: "public",
+    })
+      .populate("user")
+      .lean()
+
+    res.render("stories/index", { stories })
+  } catch (err) {
+    console.error(err)
+    res.render("error/500")
   }
 })
 
